ac254d41105b748d9de64cb16d02002dc7896679
Ciaran Maye <ciaranmaye8@gmail.com>
2024-05-22 18:39:12 +0100

    Updates to dependency updater

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import sys
 from PyQt5.QtWidgets import (
     QApplication,
     QMainWindow,
@@ -12,6 +13,7 @@ from mainwindow_ui import Ui_MainWindow
 
 class Communicate(QObject):
     output_updated = pyqtSignal(str)
+    error_message = pyqtSignal(str)
 
 
 class TrainingThread(QThread):
@@ -70,6 +72,70 @@ class TrainingThread(QThread):
         self.communicate.output_updated.emit(remaining_output)
 
 
+class UpdateYoloV5Thread(QThread):
+    def __init__(self, communicate):
+        super().__init__()
+        self.communicate = communicate
+
+    def run(self):
+        if not self.check_git_installed():
+            if not self.install_git():
+                self.communicate.error_message.emit("Failed to install git.")
+                return
+
+        current_directory = os.path.abspath(os.getcwd())
+        yolov5_path = os.path.join(current_directory, "yolov5")
+
+        # Check if the yolov5 directory exists
+        if not os.path.exists(yolov5_path):
+            try:
+                subprocess.run(
+                    ["git", "clone", "https://github.com/ultralytics/yolov5.git"],
+                    check=True,
+                )
+            except Exception as e:
+                self.communicate.error_message.emit(f"Error updating yolov5: {e}")
+                return
+        else:
+            try:
+                os.chdir(yolov5_path)
+                subprocess.run(["git", "pull"], check=True)
+            except Exception as e:
+                self.communicate.error_message.emit(
+                    f"Error updating Yolov5 repository: {e}"
+                )
+                return
+
+    def check_git_installed(self):
+        try:
+            subprocess.run(
+                ["git", "--version"],
+                check=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+            return True
+        except Exception:
+            return False
+
+    def install_git(self):
+        try:
+            if sys.platform.startswith("linux"):
+                subprocess.run(["sudo", "apt-get", "update"], check=True)
+                subprocess.run(["sudo", "apt-get", "install", "-y", "git"], check=True)
+            elif sys.platform == "darwin":  # macOS
+                subprocess.run(["brew", "install", "git"], check=True)
+            elif sys.platform.startswith("win32"):
+                subprocess.run(["winget", "install", "--id", "Git.Git"], check=True)
+            else:
+                self.communicate.error_message.emit("Unsupported operating system.")
+                return False
+            return True
+        except Exception as e:
+            self.communicate.error_message.emit(f"Error installing git: {e}")
+            return False
+
+
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
@@ -78,6 +144,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.yaml_file_button.clicked.connect(self.select_yaml_file)
         self.output_directory_button.clicked.connect(self.select_output_directory)
         self.start_training_button.clicked.connect(self.start_training)
+
+        self.communicate = Communicate()
+        self.communicate.error_message.connect(self.show_error_message)
 
         self.update_yolov5_directory()
 
@@ -138,7 +207,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             yaml_file,
             img_size,
             model_size,
-            Communicate(),
+            self.communicate,
         )
         self.training_thread.communicate.output_updated.connect(
             self.output_text_edit.append
@@ -146,41 +215,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.training_thread.start()
 
     def update_yolov5_directory(self):
-        current_directory = os.path.abspath(os.getcwd())
-        yolov5_path = os.path.join(current_directory, "yolov5")
+        self.update_thread = UpdateYoloV5Thread(self.communicate)
+        self.update_thread.start()
 
-        # Check if the yolov5 directory exists
-        if not os.path.exists(yolov5_path):
-            print("yolov5 directory not found. Cloning from repository...")
-            self.show_info_dialog("Updating", "Downloading yolov5 dependencies...")
-            try:
-                subprocess.run(
-                    ["git", "clone", "https://github.com/ultralytics/yolov5.git"]
-                )
-                print("yolov5 updated successfully.")
-            except Exception as e:
-                print(f"Error updating yolov5: {e}")
-                self.show_error_dialog("Error", "Failed to clone Yolov5 repository.")
-                return
-        else:
-            print("yolov5 directory found. Updating from repository...")
-            self.show_info_dialog("Updating", "Updating yolov5 dependencies...")
-            try:
-                os.chdir(yolov5_path)
-                subprocess.run(["git", "pull"])
-                print("yolov5 repository updated successfully.")
-            except Exception as e:
-                print(f"Error updating Yolov5 repository: {e}")
-                self.show_error_dialog("Error", "Failed to update yolov5 dependencies.")
-                return
-
-        self.show_info_dialog("Update Complete", "Yolov5 dependencies is up to date.")
-
-    def show_info_dialog(self, title, message):
-        QMessageBox.information(self, title, message)
-
-    def show_error_dialog(self, title, message):
-        QMessageBox.critical(self, title, message)
+    def show_error_message(self, message):
+        self.output_text_edit.append(f"ERROR: {message}")
 
 
 if __name__ == "__main__":
